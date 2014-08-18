0c86784ef3d406a86e1bba11b8f2db7a5dfcb618
Mathew Hembury <Astro@HomeHB.local>
2014-08-18 02:38:43 +0200

    created corr.R for R_programming course

--- corr.R
+++ corr.R
@@ -8,5 +8,24 @@ corr <- function(directory, threshold = 0) {
         ## nitrate and sulfate; the default is 0
         
         ## Return a numeric vector of correlations
-
+        
+        source("complete.R")
+        compl <- complete(directory)
+                
+        l <- length(compl$id)
+        #l2 <- length(compl$nobs[compl$nobs >= threshold])
+        #cr <- numeric(length = l)
+                        
+        for(i in 1:l) {
+                if (compl$nobs[i] > threshold) {
+                        data <- openspecdata(i = compl$id[i], directory = directory)
+                        good <- complete.cases(data)
+                        dataclean <- data[good,]
+                        cr[i]  <- cor(dataclean$sulfate, dataclean$nitrate)   
+                } else {
+                        cr[i] <- NA
+                }
+        }
+        bad <- is.na(cr)
+        cr[!bad]
 }
